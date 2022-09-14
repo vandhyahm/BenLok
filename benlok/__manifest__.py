29a2ecc8fdf30c1e36726e244e4fd539910387d4
vandhya <vandhya12@gmail.com>
2022-09-14 22:11:23 +0700

    fix bug

--- benlok/__manifest__.py
+++ benlok/__manifest__.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "benlok",
+    'name': "BenLok",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        BenLok (Bengkel Lokal) merupakan suatu sistem Enterprise Resource Planning untuk usaha 
+        bengkel lokal.""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'sequence': 0,
+
+    'author': "Arllivandhya Dani",
+    'website': "https://github.com/vandhyahm",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'ERP',
+    'application': True,
     'version': '0.1',
 
     # any module necessary for this one to work correctly
